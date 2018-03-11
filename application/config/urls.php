b54c53daf8c580ea2ba00d77ed9a3938dc7b7630
andrew <81kab@mail.ru>
2018-03-11 10:31:51 +0700

    changed page slug rule

--- application/config/urls.php
+++ application/config/urls.php
@@ -1,7 +1,7 @@
 <?php
 return [
 
-    'page/<slug:\w+>' => 'page/view',
+    'page/<slug:[_\-a-zA-Z0-9]+>' => 'page/view',
     'image/<id:\d+>-<size:\w+>-<rev:\d+>.jpg' => 'image/get',
     '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
     '<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
